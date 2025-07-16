92bd87fd9778f7303a79894c0f13905203327dec
Realitaa <rezamuliaputra927@gmail.com>
2025-07-16 15:47:37 +0700

    fix: using verifyNoteAccess to check notes and collaborated notes

--- src/api/notes/handler.js
+++ src/api/notes/handler.js
@@ -48,7 +48,7 @@ class NotesHandler {
       const { id } = request.params;
       const { id: credentialId } = request.auth.credentials;
 
-      await this.service.verifyNoteOwner(id, credentialId);
+      await this.service.verifyNoteAccess(id, credentialId);
       const note = await this.service.getNoteById(id);
 
       return {
@@ -83,7 +83,8 @@ class NotesHandler {
       this.validator.validateNotePayload(request.payload);
       const { id } = request.params;
       const { id: credentialId } = request.auth.credentials;
-      await this.service.verifyNoteOwner(id, credentialId);
+
+      await this.service.verifyNoteAccess(id, credentialId);
       await this.service.editNoteById(id, request.payload);
 
       return {
